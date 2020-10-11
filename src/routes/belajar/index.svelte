ffcded22a7570a7b0bf4dd665960794e31282f7a
Yusuf <akuyusuf27@gmail.com>
2020-10-11 09:35:05 +0700

    feat: 🎸 add learn page
    ✅ Closes: #5

---
+++ src/routes/belajar/index.svelte
@@ -0,0 +1,23 @@
+<script>
+  import BoxLearning from '../../components/box-learning.svelte';
+  import learnings from './_data.yml';
+</script>
+
+<style>
+  .heading {
+    @apply mt-0;
+  }
+</style>
+
+<section class="wrapper prose prose-lg bg-gray-100">
+  <header class="text-center">
+    <h6 class="uppercase">Mau Belajar Svelte?</h6>
+    <h2 class="heading">Materi Pembelajaran</h2>
+  </header>
+
+  <section class="sm:grid sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+    {#each learnings as learning}
+      <BoxLearning {...learning} />
+    {/each}
+  </section>
+</section>
