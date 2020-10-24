29fa94668ccca8a6b0d6c20ca616039ec9884d95
iamyuu <akuyusuf27@gmail.com>
2020-10-24 21:58:39 +0700

    chore: add env example

--- src/constants/eventbrite.js
+++ src/constants/eventbrite.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://www.eventbriteapi.com/v3';
-export const TOKEN = ENV.EVENTBRITE_TOKEN;
+export const TOKEN = ENV.EVENTBRITE_TOKEN || '';
 export const ORGANIZATION_ID = '478633463233'; // svelte id
