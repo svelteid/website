e9e815c0bc98825993b1ceed88c3f2a3ae0c85c7
Alfin <m.alfin.syahruddin@gmail.com>
2020-10-26 12:52:46 +0700

    Halaman Ajukan Topik

--- src/routes/ajukan-topik/_layout.svelte
+++ src/routes/ajukan-topik/_layout.svelte
@@ -18,7 +18,7 @@
   <div class="md:flex">
     <div class="xs:w-full xs:mb-6 md:w-1/2 md:mr-12">
       <div class="illustration">
-        <Illustration class="w-full" />
+        <Illustration />
       </div>
     </div>
     <div class="xs:w-full md:w-1/2">
