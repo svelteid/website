df482e85ec66aee9397f09722ea01f1a87f975e4
iamyuu <akuyusuf27@gmail.com>
2020-11-05 05:51:04 +0700

    refactor: learning page

--- src/routes/belajar/index.svelte
+++ src/routes/belajar/index.svelte
@@ -1,21 +1,15 @@
 <script>
-  import BoxLearning from '../../components/box-learning.svelte';
+  import BoxLearning from '~/components/ui/box-learning.svelte';
   import learnings from './_data.yml';
 </script>
 
-<style>
-  .heading {
-    @apply mt-0;
-  }
-</style>
-
-<section class="wrapper prose prose-lg bg-gray-100">
+<section class="container prose prose-lg bg-gray-100 min-h-screen py-10 mt-1">
   <header class="text-center">
-    <h6 class="uppercase">Mau Belajar Svelte?</h6>
+    <span class="uppercase">Mau Belajar Svelte?</span>
     <h2 class="heading">Materi Pembelajaran</h2>
   </header>
 
-  <section class="sm:grid sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+  <section class="grid grid-flow-row gap-4 md:grid-cols-2 lg:grid-cols-3">
     {#each learnings as learning}
       <BoxLearning {...learning} />
     {/each}
